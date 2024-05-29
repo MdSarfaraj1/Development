1cb086f24be0f02bb34cd8c32fddff94a646d20e
MdSarfaraj1 <mahammadsarfaraj6@gmail.com>
2024-05-30 01:22:44 +0530

    Web development stuff

---
+++ java script/intro.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+console.log("hello world",3+4);
+let a=5;
+let b=3;
+console.log(a+b);
+let ball=5;
+let bat=54;
+///let output="the total price is:"+(ball+bat)+"rupees";
+//same can be written as
+let output=`the total price is:${ball+bat} rupees`;
+console.log(output);
